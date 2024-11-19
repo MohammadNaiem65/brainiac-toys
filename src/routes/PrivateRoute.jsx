a8f5d39df2cbba39f288bf28bea48827c0db2b10
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-11-20 03:10:08 +0600

    fix: solved page blank issue of PrivateRoute

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -3,22 +3,23 @@ import { AuthContext } from '../providers/AuthProvider/AuthProvider';
 import { Navigate, useLocation } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
-	const { user, loggedIn, loading } = useContext(AuthContext);
-	const location = useLocation();
-	// Return if user data is loading
-	if (loading) {
-		return;
-	}
+    const { user, loggedIn, loading } = useContext(AuthContext);
+    const location = useLocation();
 
-	// Return to desired path if user data is found
-	else if (user && loggedIn) {
-		return children;
-	}
+    // Return if user data is loading
+    if (loading) {
+        return;
+    }
 
-	// If not found, return to login page
-	else if (!loading && user) {
-		return <Navigate to='/login' state={{ from: location }} replace />;
-	}
+    // Return to desired path if user data is found
+    else if (user && loggedIn) {
+        return children;
+    }
+
+    // If user data not found, return to login page
+    else if (!loading && !user) {
+        return <Navigate to='/login' state={{ from: location }} replace />;
+    }
 };
 
 export default PrivateRoute;
